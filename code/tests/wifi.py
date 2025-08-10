a3d936b89d4d0f2e452db7287833e963b2209122
hssboston <hanna.s.suzuki@gmail.com>
2025-08-10 09:07:10 -0400

    Update wifi.py

--- code/tests/wifi.py
+++ code/tests/wifi.py
@@ -1,5 +1,6 @@
 import wifi, os
 
-wifi.radio.connect(ssid=os.getenv('CIRCUITPY_WIFI_SSID'),
-                   password=os.getenv('CIRCUITPY_WIFI_PASSWORD'))
+wifi.radio.connect(
+    ssid=os.getenv("CIRCUITPY_WIFI_SSID"),
+    password=os.getenv("CIRCUITPY_WIFI_PASSWORD"))
 print("my IP addr:", wifi.radio.ipv4_address)
